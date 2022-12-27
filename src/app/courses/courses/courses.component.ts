e6edac5cb8d45102e691fcfb170043827119ea7b
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-12-27 18:49:04 -0300

    Criando Material Table para Listar Cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Course } from './../model/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent{
+
+courses: Course[] = [
+  { _id: '1', name: 'Angular', category: 'front-end'}
+];
+displayedColumns = ['name', 'category'];
 
 }
