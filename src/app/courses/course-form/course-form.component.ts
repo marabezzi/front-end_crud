68e80f1a8e71bd8dfa4bda842df2215a0d07c13a
Guilherme A. Marabezzi Clerice <marabezzi@yahoo.com.br>
2022-12-28 13:50:59 -0300

    Formulário: Salvando os dados com HTTP POST + tratamento de erros

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,8 @@
-import { CategoryPipe } from './../../shared/pipes/category.pipe';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -11,12 +13,25 @@ export class CourseFormComponent {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder,
+              private service: CoursesService,
+              private snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
     });
   }
 
+  onSubmit(){
+    this.service.save(this.form.value)
+      .subscribe(result => console.log(result), error => this.onError());
+  }
 
+  onCancel(){
+
+  }
+
+  private onError(){
+    this.snackBar.open('Erro ao salvar curso.','',{ duration:5000 });
+  }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { delay, first, tap } from 'rxjs';
+import { first, tap } from 'rxjs';
 
 import { Course } from './../model/course';
 
@@ -19,5 +19,9 @@ export class CoursesService {
       first(),
       tap(courses => console.log(courses))
     );
+  }
+
+  save(record: Course){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 }
